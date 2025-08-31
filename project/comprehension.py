4abc6295593a5b5f710b3adf5a52c72d2b938328
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-08-31 12:02:37 +0530

    added new code for generator to list

--- project/comprehension.py
+++ project/comprehension.py
@@ -1,5 +1,5 @@
 from functools import reduce
 
-print(list(x for x in range(1, 11)))
+print(reduce(lambda x, y : x * y ,list(x for x in range(1, 12) if x % 2 != 0)))
 
 print(reduce(lambda x, y : x * y, (x for x in range(1, 11) if x %2 == 0)))
