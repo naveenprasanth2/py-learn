29cffbb1ede0907ac47526cdb148157cd137f310
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-09-01 17:35:40 +0530

    modified logic to make generator more inclusive on numbers

--- project/comprehension.py
+++ project/comprehension.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
-print(reduce(lambda x, y : x * y ,list(x for x in range(1, 12) if x % 2 != 0)))
 
-print(reduce(lambda x, y : x * y, (x for x in range(1, 11) if x %2 == 0)))
+if __name__ == '__main__':
+    print(reduce(lambda x, y : x * y, (x * x for x in range(1, 12) if x % 2 == 0)))
+    print(reduce(lambda x, y : x * y ,(x ** x for x in range(1, 12) if x % 2 != 0)))
