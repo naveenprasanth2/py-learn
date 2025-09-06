f69ab97b06f0fb59b7c4da94a2a661c50ce8491f
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-09-06 11:35:33 +0530

    added type as well to the method for better clarity

--- project/comprehension.py
+++ project/comprehension.py
@@ -2,4 +2,4 @@ from functools import reduce
 
 
 if __name__ == '__main__':
-    print(reduce(lambda x, y : x * y, (x * x for x in range(1, 10) if x % 2 == 0)))
+    print(reduce(lambda x, y : x * y, (x * x for x in range(1, 11) if x % 2 == 0)))
--- project/employee_eval.py
+++ project/employee_eval.py
@@ -6,3 +6,10 @@ employee2 = Employee("prasanth", 30, 10000.00)
 
 
 print(employee1 + employee2)
+
+def test(a : int , b : int) -> int:
+    return a + b
+
+
+if __name__ == '__main__':
+    print(test(1, 2))
